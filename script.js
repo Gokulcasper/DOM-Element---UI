83edfa7f5d14253e04ca1f25fe9a81e321605bb4
Gokulcasper <ronitcasper215@gmail.com>
2022-06-15 22:59:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// How to select DOM Elements
+
+// 1. getElementById()
+// 2. getElementsByClassName()
+// 3. getElementsByTagName()
+// 4. Finding Elements by css Selectors
+// 5. querySelector and querySelectorAll()
